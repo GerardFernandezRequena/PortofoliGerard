dd2bdd111554b7704ba6e7e47c6688418b9aa91f
geroUni <gerardfernandezrequena@gmail.com>
2025-09-16 23:59:47 +0200

    `Refactor HTML and JavaScript files: replace direct HTML includes with JavaScript fetch requests, update CSS file paths, and add custom JavaScript files for form validation and header/footer loading.`

--- assets/js/About.js
+++ assets/js/About.js
@@ -124,3 +124,13 @@ document.addEventListener('DOMContentLoaded', function () {
         // import('loading-attribute-polyfill');
     }
 });
+
+// Cargar header
+fetch('header.html')
+    .then(response => response.text())
+    .then(data => document.getElementById('header').innerHTML = data);
+
+// Cargar footer
+fetch('footer.html')
+    .then(response => response.text())
+    .then(data => document.getElementById('footer').innerHTML = data);
--- assets/js/paginaPrincipal.js
+++ assets/js/paginaPrincipal.js
@@ -59,3 +59,13 @@ document.addEventListener('DOMContentLoaded', function () {
         }
     });
 });
+
+// Cargar header
+fetch('header.html')
+    .then(response => response.text())
+    .then(data => document.getElementById('header').innerHTML = data);
+
+// Cargar footer
+fetch('footer.html')
+    .then(response => response.text())
+    .then(data => document.getElementById('footer').innerHTML = data);
